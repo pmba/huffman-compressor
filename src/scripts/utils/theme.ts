10e8accc0cb27e74186d369b40f65f66cdc0e780
Phyllipe Bezerra <phyllipe.bezerra@gmail.com>
2023-04-11 00:13:41 -0300

    feat: add theme button, favicon and title

--- src/scripts/utils/theme.ts
+++ src/scripts/utils/theme.ts
@@ -1,27 +1,36 @@
-function setupTheme() {
+const themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon")!;
+const themeToggleLightIcon = document.getElementById(
+  "theme-toggle-light-icon"
+)!;
+const themeToggleBtn = document.getElementById(
+  "theme-toggle"
+)! as HTMLButtonElement;
+
+type ThemeScheme = "light" | "dark";
+
+export function setupTheme() {
   if (
     localStorage.theme === "dark" ||
     (!("theme" in localStorage) &&
       window.matchMedia("(prefers-color-scheme: dark)").matches)
   ) {
+    localStorage.theme = "dark";
     document.documentElement.classList.add("dark");
+    themeToggleLightIcon.classList.remove("hidden");
   } else {
+    localStorage.theme = "light";
     document.documentElement.classList.remove("dark");
+    themeToggleDarkIcon.classList.remove("hidden");
   }
 }
 
-type ThemeScheme = "light" | "dark";
+themeToggleBtn.onclick = function () {
+  themeToggleDarkIcon.classList.toggle("hidden");
+  themeToggleLightIcon.classList.toggle("hidden");
 
-export function setTheme(theme: ThemeScheme) {
-  localStorage.theme = theme;
-}
+  localStorage.theme = localStorage.theme === "light" ? "dark" : "light";
 
-export function osPreference(os: boolean) {
-  if (!os) {
-    return setupTheme();
-  }
-
-  localStorage.removeItem("theme");
-}
+  setupTheme();
+};
 
 setupTheme();
